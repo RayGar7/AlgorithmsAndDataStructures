c6b34c559f2ab52da7222bf0a580b4231b485189
Ray <rgarcia.7770@gmail.com>
2020-11-23 20:51:05 -0600

    Added the solution for first recurring character in a string and a diagram for it

---
+++ Python/first_recurring_character/first_recurring_character.py
@@ -0,0 +1,29 @@
+# return the first occurence of a character in a string, if there are no duplicate characters in a string return None
+# O(n) 
+def main(s):
+    my_list = []
+    for c in s:
+        if c not in my_list:
+            my_list.append(c)
+        else:
+            return c
+
+    return None
+
+
+#print(main("ACB"))
+
+# same problem but in place
+# O(n)
+def in_place(s):
+    i = 1
+    while i < len(s):
+        j = 0
+        while j < i:
+            if (s[i] == s[j]):
+                return s[i]
+            j += 1
+        i +=1
+    return None
+
+print(in_place("BCAA"))
