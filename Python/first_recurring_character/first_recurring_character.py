08989c70af8cc12fb26f6479b81cd3c8733f64ad
Ray <rgarcia.7770@gmail.com>
2021-05-15 18:05:37 -0500

    Better usage of the dictionary built-in methdos

--- Python/first_recurring_character/first_recurring_character.py
+++ Python/first_recurring_character/first_recurring_character.py
@@ -11,19 +11,42 @@ def main(s):
     return None
 
 
-#print(main("ACB"))
+#print(main("ACCB"))
 
 # same problem but in place
 # O(n^2)
+# def in_place(s):
+#     i = 1
+#     while i < len(s):
+#         j = 0
+#         while j < i:
+#             if (s[i] == s[j]):
+#                 return s[i]
+#             j += 1
+#         i +=1
+#     return None
+
+# this is the same implementation as the previous problem but with a shorter syntax
 def in_place(s):
-    i = 1
-    while i < len(s):
-        j = 0
-        while j < i:
+    for i in range(1, len(s)):
+        for j in range(0, i):
             if (s[i] == s[j]):
                 return s[i]
-            j += 1
-        i +=1
+    return None
+    
+# the same problem but with a hash map
+# note: geeksforgeeks calls it a hash map but there's no hash function so I don't understand how it can be a hash map
+def firstRepeatedChar(str): 
+    h = {} 
+ 
+    for ch in str:
+        if ch in h.keys():
+            return ch
+        else:
+            h[ch] = 1
+ 
     return None
 
-print(in_place("BCAA"))
+print(firstRepeatedChar("BCAA"))
+print(firstRepeatedChar("B"))
+print(firstRepeatedChar("BB"))
